80426d8eb332ecdabc83942f788ff2723b699fca
Aubrey Snider <aubrey@sni.dev>
2020-07-30 18:21:08 -0500

    express server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
+const path = require('path')
 const express = require('express')
 const app = express()
-const port = 3000
-
-app.use(express.static('build'))
+const port = process.env.PORT || 3000;
 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const publicPath = path.join(__dirname, '..', 'public')
+console.log(publicPath)
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`)
+})
